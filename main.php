9a09c9530fb2f2f4c0603e43a4b159b0e1ae9ccb
Dang Thi Thu Nga <dang.thunga1105@gmail.com>
2022-01-23 18:07:45 +0700

    Add function Upgrade plugin
    Add new function : Upgrade for plugin

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Moomoo Extensions Elementor
 Plugin URI: --
 Description: Extensions for Elementor: buttons, media, slides, gravityform style ...
-Version: 1.2.3
+Version: 1.2.4
 Author: Ngadt
 License: GPLv2 or later
 Text Domain: moomoo.sg
@@ -11,10 +11,16 @@ Text Domain: moomoo.sg
 
 define("MM_EXT_ASSET_PATH", plugin_dir_path(__FILE__).'/assets');
 define("MM_EXT_ASSET_URL",  plugins_url('assets' ,__FILE__)); 
+define("MM_PLUGIN_NAME", basename(dirname(__FILE__)));
+define("MM_PLUGIN_NAME_MAIN_FILE",MM_PLUGIN_NAME.'/'.basename(__FILE__));
+
 
 require_once('class/meta-box.php');
 require_once('extensions/column-extended.php');
 require_once('extensions/element-extended.php');
+require_once('upgrade.php');
+
+
 
 
 
--- readme.txt
+++ readme.txt
@@ -6,3 +6,6 @@ Stable tag: 3.0.1
 License: GPLv2 or later
 
 The Categories Moomoo Extensions Elementor extend some function for Elementor: posts layout, gravityform style, sliders, buttons, media ...
+
+=== Version: 1.2.4 ===
+- Add function auto upgrade plugin
